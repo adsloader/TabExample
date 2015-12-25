073199e707b2de4d18b16af18fcf1d4c52c4de39
park sungwoan <park sungwoan>
2015-12-25 17:27:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/park/tabexample/contactActivity.java
@@ -0,0 +1,18 @@
+package com.example.park.tabexample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class contactActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView 실행 전에 코딩되어야 한다. 그리고 Activity만 된다.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_contact);
+    }
+}
---
+++ app/src/main/java/com/example/park/tabexample/historyActivity.java
@@ -0,0 +1,14 @@
+package com.example.park.tabexample;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class historyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
