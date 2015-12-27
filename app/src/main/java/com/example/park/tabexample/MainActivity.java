0cc90a8a4a32937b8a9df3707c1df558d08f8f89
park <adsloader99@gmail.com>
2015-12-27 13:01:49 +0900

    blink 추가

--- app/src/main/java/com/example/park/tabexample/MainActivity.java
+++ app/src/main/java/com/example/park/tabexample/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.park.tabexample;
 
 import android.app.TabActivity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
 
--- app/src/main/java/com/example/park/tabexample/contactActivity.java
+++ app/src/main/java/com/example/park/tabexample/contactActivity.java
@@ -1,7 +1,6 @@
 package com.example.park.tabexample;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
